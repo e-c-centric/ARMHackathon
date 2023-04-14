dcfec5431a38ef53e4ab0b07ea33e4cc1d27e946
Elikem Asudo <92120198+e-c-centric@users.noreply.github.com>
2023-04-14 18:58:45 +0000

    Add files via upload

---
+++ ARMHackathon.ino
@@ -0,0 +1,140 @@
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+#include <DHT.h>
+#include <RTClib.h>
+
+#define DHTTYPE DHT22
+#define DHTPIN 2
+#define SOILPIN A0
+#define LDRPIN A1
+#define BUZZERPIN 3
+#define LEDPIN 13
+#define I2C_ADDR 0x27
+
+LiquidCrystal_I2C lcd(I2C_ADDR,16,2);
+DHT dht(DHTPIN, DHTTYPE);
+RTC_DS1307 rtc;
+
+
+void displayTime() {
+  DateTime now = rtc.now();
+  lcd.setCursor(0, 0);
+  lcd.print("Time: ");
+  lcd.print(now.hour(), DEC);
+  lcd.print(':');
+  lcd.print(now.minute(), DEC);
+  lcd.print(':');
+  lcd.print(now.second(), DEC);
+}
+void displayTempHum() {
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+  lcd.setCursor(0, 1);
+  lcd.print("Temp: ");
+  lcd.print(t);
+  lcd.print((char)223);
+  lcd.print("C");
+  lcd.print(" Humidity: ");
+  lcd.print(h);
+  lcd.print("%");
+}
+void displaySoilMoisture() {
+  lcd.clear();
+  int soilMoisture = analogRead(SOILPIN);
+  lcd.setCursor(0, 2);
+  lcd.print("Soil Moisture:");
+  lcd.print(soilMoisture);
+}
+
+void checkTemp() {
+  float t = dht.readTemperature();
+  if (t >= 30) {
+    digitalWrite(LEDPIN, HIGH);
+    tone(BUZZERPIN, 1000, 1000);
+  } else {
+    digitalWrite(LEDPIN, LOW);
+    noTone(BUZZERPIN);
+  }
+}
+
+void checkHumidity() {
+  float h = dht.readHumidity();
+  if (h <= 30) {
+    digitalWrite(LEDPIN, HIGH);
+    tone(BUZZERPIN, 1000, 1000);
+  } else {
+    digitalWrite(LEDPIN, LOW);
+    noTone(BUZZERPIN);
+  }
+}
+void checkSoilMoisture() {
+  int soilMoisture = analogRead(SOILPIN);
+  if (soilMoisture < 300) {
+    digitalWrite(LEDPIN, HIGH);
+    tone(BUZZERPIN, 1000, 1000);
+  } else {
+    digitalWrite(LEDPIN, LOW);
+    noTone(BUZZERPIN);
+  }
+}
+void checkLight() {
+  int lightLevel = analogRead(LDRPIN);
+  if (lightLevel < 100) {
+    digitalWrite(LEDPIN, HIGH);
+    tone(BUZZERPIN, 1000, 1000);
+  } else {
+    digitalWrite(LEDPIN, LOW);
+    noTone(BUZZERPIN);
+  }
+}
+void displayLight() {
+  int lightLevel = analogRead(LDRPIN);
+  lcd.setCursor(0, 3);
+  lcd.print("Light Level: ");
+  lcd.print(lightLevel);
+}
+ 
+
+void setup() {
+  pinMode(LEDPIN, OUTPUT);
+  pinMode(BUZZERPIN, OUTPUT);
+  Serial.begin(9600);
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("Initializing...");
+  delay(2000);
+  dht.begin();
+  Wire.begin();
+  rtc.begin();
+//  if (! rtc.isrunning()) {
+//    lcd.setCursor(0, 0);
+//    lcd.print("RTC is NOT running!");
+//    rtc.adjust(DateTime(__DATE__, __TIME__));
+//  }
+  lcd.clear();
+  lcd.print("System ready!");
+}
+void loop() {
+  delay(5000);
+  lcd.clear();
+  checkTemp();
+  checkHumidity();
+  checkSoilMoisture();
+  checkLight();
+  
+  displayTime();
+  delay(2000);
+  displayTempHum();
+  delay(2000);
+  displaySoilMoisture();
+  delay(2000);
+  displayLight();
+  delay(2000);
+  lcd.clear();
+  delay(1000);
+}
